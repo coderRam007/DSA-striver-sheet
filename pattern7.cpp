b94c2b9a5d4371e0f5a52ff257db795dff35f100
coderRam007 <200701191@rajalakshmi.edu.in>
2023-10-22 22:47:44 +0530

    pattern7

--- pattern7.cpp
+++ pattern7.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"hello";
+    int n;
+    cin>>n;
+    for(int i=0;i<=n;i++){
+        for(int j=n-i;j>=1;j--)
+        cout<<" ";
+        for(int j=0;j<i;j++)
+        cout<<"*"<<" ";
+        cout<<"\n";
+    }
     return 0;
 }
